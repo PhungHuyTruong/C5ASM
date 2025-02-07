999e5762a8a3ab9a6195f0ff1a5f47d02834cab9
PhụngHuyTruong <truongphph48831@gmail.com>
2025-02-07 15:38:35 +0700

    Cập nhật gd2

--- PH48831_C5_ASM/Program.cs
+++ PH48831_C5_ASM/Program.cs
@@ -22,25 +22,17 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
-    // Các tùy chọn về mật khẩu
     options.Password.RequireDigit = false;
     options.Password.RequiredLength = 6;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireUppercase = false;
     options.Password.RequireLowercase = false;
 
-    //// Cấu hình khóa tài khoản
-    //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    //options.Lockout.MaxFailedAccessAttempts = 5;
-    //options.Lockout.AllowedForNewUsers = true;
-
-    //// Cấu hình user
-    //options.User.RequireUniqueEmail = true;
 });
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
-    options.LoginPath = "/Account/Login"; // Đường dẫn đến trang login
+    options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
 });
 
@@ -51,19 +43,16 @@ using (var scope = app.Services.CreateScope())
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-    // Kiểm tra và tạo vai trò "Admin" nếu chưa có
     if (!await roleManager.RoleExistsAsync("Admin"))
     {
         await roleManager.CreateAsync(new IdentityRole("Admin"));
     }
 
-    // Kiểm tra nếu người dùng chưa tồn tại
     var email = "admin11@gmail.com";
     var user = await userManager.FindByEmailAsync(email);
 
     if (user == null)
     {
-        // Tạo người dùng mới với các thuộc tính bổ sung
         var adminUser = new ApplicationUser
         {
             UserName = email,
@@ -71,16 +60,14 @@ using (var scope = app.Services.CreateScope())
             FullName = "Admin User",
             PhoneNumber = "123456789",
             Address = "123 Admin St",
-            DateOfBirth = new DateTime(1990, 1, 1),  // Ngày sinh admin
+            DateOfBirth = new DateTime(1990, 1, 1),  
             EmailConfirmed = true
         };
 
-        // Thêm tài khoản với mật khẩu
         var result = await userManager.CreateAsync(adminUser, "admin123");
 
         if (result.Succeeded)
         {
-            // Gán vai trò "Admin" cho người dùng
             await userManager.AddToRoleAsync(adminUser, "Admin");
         }
     }
